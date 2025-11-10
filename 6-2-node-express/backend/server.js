6c2fd0df0a742db231a43e1f55595a5140f2e914
darkwinTech <s202257160@kfupm.edu.sa>
2025-11-10 11:20:56 +0300

    server part

--- 6-2-node-express/backend/server.js
+++ 6-2-node-express/backend/server.js
@@ -2,11 +2,8 @@ import express from "express";
 import cors from "cors";
 import { getRandomQuote } from "./quotes.js";
 
-{/*write code for cors*/}
 const app = express();
 
-
-{/*write code to define routes*/}
 app.use(cors());
 app.get("/", (_req, res) => {
     res.send("Quote Generator API");
@@ -16,7 +13,7 @@ app.get("/api/quote", (_req, res) => {
     const quote = getRandomQuote();
     res.json({ quote });
 });
-{/*write code to create server*/}
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server listening on http://localhost:${PORT}`);
