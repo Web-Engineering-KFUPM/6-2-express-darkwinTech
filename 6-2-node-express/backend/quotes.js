6348a29c1f586955cc4d6576cc2d6e2a0563698d
darkwinTech <s202257160@kfupm.edu.sa>
2025-11-10 11:20:17 +0300

    getRandomQuote

--- 6-2-node-express/backend/quotes.js
+++ 6-2-node-express/backend/quotes.js
@@ -8,6 +8,6 @@ const quotes = [
 ];
 
 export function getRandomQuote() {
-    const idx = Math.floor(Math.random() * quotes.length);
+    const idx = getRandomInt(quotes.length);
  return quotes[idx];
 }
--- 6-2-node-express/backend/server.js
+++ 6-2-node-express/backend/server.js
@@ -3,9 +3,21 @@ import cors from "cors";
 import { getRandomQuote } from "./quotes.js";
 
 {/*write code for cors*/}
+const app = express();
 
 
 {/*write code to define routes*/}
+app.use(cors());
+app.get("/", (_req, res) => {
+    res.send("Quote Generator API");
+});
 
-
+app.get("/api/quote", (_req, res) => {
+    const quote = getRandomQuote();
+    res.json({ quote });
+});
 {/*write code to create server*/}
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server listening on http://localhost:${PORT}`);
+});
